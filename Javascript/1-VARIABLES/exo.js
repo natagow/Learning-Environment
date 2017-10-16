412a42459bdf7c4dbccb967a28cfca1c477173b2
Trabelsi <trabelsi.mehdi@hotmail.com>
2017-10-16 12:21:19 +0200

    Exo 1 du 1

---
+++ Javascript/1-VARIABLES/exo.js
@@ -0,0 +1,8 @@
+/* Exo 1 bonne aventure */
+var NbEnf= prompt("Quel est le nombre d'enfant ?");
+var NomFemme= prompt("Quel est le nom de votre femme ?");
+var Pays= prompt("D'où venez-vous ?");
+var Metier= prompt("Quel est votre métier?");
+console.log(" Ma femme s'appelle "  +  NomFemme  + " J'ai " +  NbEnf  + " Je viens de " + Pays +  " Mon métier est " +  Metier);
+
+/*Exo 2 
