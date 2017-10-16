683f2075c41133a855d20e9ac27036565dbed96b
Trabelsi <trabelsi.mehdi@hotmail.com>
2017-10-16 12:31:58 +0200

    modif exo 1

---
+++ Javascript/1-VARIABLES/Calculer l'âge/exo2.js
@@ -0,0 +1,4 @@
+/*Exo 2 Calcul de l'âge */
+var AnnéeAct= prompt("En quel année sommes-nous ?");
+var AnnéeNai= prompt("Quel est votre année de naissance ?");
+console.log(" Nous sommes en " + AnnéeAct + " La personne est née en " + AnnéeNai )
--- Javascript/1-VARIABLES/Diseur de bonne aventure/exo1.js
+++ Javascript/1-VARIABLES/Diseur de bonne aventure/exo1.js
@@ -4,5 +4,3 @@ var NomFemme= prompt("Quel est le nom de votre femme ?");
 var Pays= prompt("D'où venez-vous ?");
 var Metier= prompt("Quel est votre métier?");
 console.log(" Ma femme s'appelle "  +  NomFemme  + " J'ai " +  NbEnf  + " Je viens de " + Pays +  " Mon métier est " +  Metier);
-
-/*Exo 2 
