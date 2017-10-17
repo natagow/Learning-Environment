d1ebfd5ac43704120c31cd813bf85fbc9173d08a
Trabelsi <trabelsi.mehdi@hotmail.com>
2017-10-17 19:06:40 +0200

    Transfert vers un autre repo

--- Javascript/1-VARIABLES/Calculer l'âge/exo2.js
+++
@@ -1,4 +0,0 @@
-/*Exo 2 Calcul de l'âge */
-var AnnéeAct= prompt("En quel année sommes-nous ?");
-var AnnéeNai= prompt("Quel est votre année de naissance ?");
-console.log(" Nous sommes en " + AnnéeAct + " La personne est née en " + AnnéeNai )
